88bfe36d58ef9c3bf68d9110dd860c45cd29bf5b
Artem Khametov <khametovartem@gmail.com>
2023-10-06 17:02:48 +0300

    Начало проекта PhonoText(Vue)

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <header>
+      <!-- Здесь ваш заголовок -->
+    </header>
+    <ul>
+      <item-list
+        v-for="(item, index) in items"
+        :key="index"
+        :itemName="item.name"
+        :itemText="item.text"
+      >
+        <!-- Передаем текст элемента в компонент ItemList через props -->
+      </item-list>
+    </ul>
+    <footer>
+      <!-- Здесь ваш футер -->
+    </footer>
+  </div>
+</template>
+
+<script>
+import ItemList from "@/components/ItemList.vue"; // Импортируем компонент ItemList
+
+export default {
+  components: {
+    ItemList, // Регистрируем компонент ItemList для использования в шаблоне
+  },
+  data() {
+    return {
+      items: [
+        { name: "Элемент 1", text: "Описание 1" },
+        { name: "Элемент 2", text: "Описание 2" },
+        { name: "Элемент 3", text: "Описание 3" },
+      ], // Массив объектов, содержащий элементы и описания
+    };
+  },
+};
+</script>
+
+<style>
+/* Здесь можно добавить глобальные стили для вашего приложения */
+</style>
---
+++ src/components/ItemList.vue
@@ -0,0 +1,24 @@
+<template>
+  <li>
+    <!-- Отображаем текст элемента, переданный через props -->
+    <h2>
+      {{ itemName }}
+    </h2>
+    <p>
+      {{ itemText }}
+    </p>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    itemName: String, // Принимаем текст элемента через props
+    itemText: String, // Принимаем текст элемента через props
+  },
+};
+</script>
+
+<style scoped>
+/* Здесь можно добавить стили для компонента ItemList */
+</style>
