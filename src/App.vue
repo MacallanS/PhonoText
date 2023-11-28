2fdbaae0b7a7fefb476090b50ae2d5202a8115a0
oliapekunova@gmail.com <khametovartem@gmail.com>
2023-11-28 11:31:53 +0300

    Выбор стихов

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,18 @@
   <div class="app">
     <Header class="app__header" />
     <main class="app__main main__wrapper">
-      <ul class="main__list">
-        <item-list
-          v-for="(message, index) in messages"
-          :key="index"
-          :message="message"
-          class="main__item"
-        >
-          <button @click="deleteMessage(index)" class="delete-button">Удалить</button>
-        </item-list>
-      </ul>
-      <button @click="replaceMessages" class="button app__button">Заменить строки</button>
-      <button @click="deleteMessage" class="button app__button">Удалить строки</button>
+      <div class="info">
+        <ul class="main__list">
+          <item-list
+            v-for="(message, index) in messages"
+            :key="index"
+            :message="message"
+            class="main__item"
+          >
+          </item-list>
+        </ul>
+      </div>
+      <div class="poems"><Poems></Poems></div>
     </main>
     <Footer class="app__footer" />
   </div>
@@ -23,13 +23,14 @@
 import ItemList from "@/components/ItemList.vue";
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
+import Poems from "@/components/Poems.vue";
 import axios from "axios";
-
 export default {
   components: {
     ItemList,
     Header,
     Footer,
+    Poems,
   },
   data() {
     return {
@@ -40,31 +41,42 @@ export default {
   mounted() {
     axios
       .get("http://localhost:8088")
-      .then((response) => {
+      .then(async (response) => {
         this.messages = response.data.messages;
         this.data = response.data;
+
+        await this.executeActionsWithDelay();
       })
       .catch((error) => {
         console.error(error);
       });
   },
   methods: {
-    replaceMessages() {
+    async executeActionsWithDelay() {
+      await this.replaceMessagesWithDelay();
+      await this.deleteMessagesWithDelay();
+    },
+    async replaceMessagesWithDelay() {
       for (const key in this.data.changes.replace.info) {
         const changeInfo = this.data.changes.replace.info[key];
         const indexToReplace = changeInfo.index;
         const newText = changeInfo.text;
 
+        await this.delay(1000);
         this.messages[indexToReplace] = newText;
       }
     },
-    deleteMessage() {
+    async deleteMessagesWithDelay() {
       const deleteIndices = this.data.changes.delete;
       deleteIndices.sort((a, b) => b - a);
 
       for (const deleteIndex of deleteIndices) {
+        await this.delay(1000);
         this.messages.splice(deleteIndex, 1);
       }
+    },
+    delay(ms) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
     },
   },
 };
@@ -76,7 +88,13 @@ export default {
   margin: 0;
 }
 
+ul {
+  list-style: none;
+}
+
 .main__wrapper {
+  display: flex;
+  justify-content: space-between;
   margin: 0 auto;
   padding: 20px 70px;
   min-height: 683px;
@@ -111,5 +129,14 @@ export default {
 
 .delete-button:hover {
   background-color: #ff3333;
+}
+
+.info {
+  width: 80%;
+}
+
+.poems {
+  order: 1;
+  margin-left: auto;
 }
 </style>
---
+++ src/components/Poems.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul>
+    <li>
+      <label for="1">
+        <input type="radio" id="1" name="poem" />
+        Стих 1
+      </label>
+    </li>
+    <li class="poem">
+      <label for="2">
+        <input type="radio" id="2" name="poem" />
+        Стих 2
+      </label>
+    </li>
+    <li>
+      <label for="3">
+        <input type="radio" id="3" name="poem" />
+        Стих 3
+      </label>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Poems",
+};
+</script>
+
+<style scoped>
+.poem {
+  display: flex;
+}
+</style>
